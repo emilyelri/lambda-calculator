a3c788d1bb75fd451d0d35619ee846720902865a
Emily_Richard <richard.emily.e@gmail.com>
2019-09-10 14:58:14 -0400

    Completed steps 1 & 2

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
+import OperatorButton from "./NumberButton";
+
 //Import your array data to from the provided data file
+
+import { operators } from "../../../data";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorButtons, setOperatorButtons] = useState(operators);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
+import SpecialsButtons from "./SpecialButton";
+
 //Import your array data to from the provided data file
+
+import { specials } from "../../../data";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialsButtons, setSpecialsButtons] = useState(specials);
 
   return (
     <div>
